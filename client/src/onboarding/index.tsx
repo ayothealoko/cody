263179309b46fb1e349aa4868e2cee824bf26399
monoblack21 <monoblack21@gmail.com>
2021-05-21 11:18:18 -0400

    Create onboarding page

--- client/src/onboarding/index.tsx
+++ client/src/onboarding/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Onboarding() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <h1 className="header__h1">Cody</h1>
+      </header>
+      <div>
+        <div className="tutorial"></div>
+        <Link to="/app" className="large-button center-block no-link-style">
+          Go to app
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Onboarding;
